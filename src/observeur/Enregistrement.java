d82ce08797215ff3e2d2a26b4695f9fc3b0bf26a
Robin Louarn <robin.louarn@icloud.com>
2018-12-07 12:04:52 +0100

    modification obsserveur (ne marche pas)

---
+++ src/observeur/Enregistrement.java
@@ -0,0 +1,9 @@
+package observeur;
+
+import java.lang.reflect.Method;
+
+public class Enregistrement {
+    Observeur observeur;
+
+    Method method;
+}
--- src/observeur/Observable.java
+++ src/observeur/Observable.java
@@ -1,5 +1,6 @@
 package observeur;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,15 +8,44 @@ import java.util.Map;
 
 public class Observable {
 
-    private Map<Class<? extends Evenement>, List<Observeur>> observeursMap;
+    private Map<Class<? extends Evenement>, Map<Method, List<Observeur>>> evenementMap;
+
 
     public Observable() {
-        this.observeursMap = new HashMap<>();
+        this.evenementMap = new HashMap<>();
     }
 
+
+
+    public void performRecord(Class<? extends Evenement> evtClass, Observeur o, Method method) {
+        Map<Method, List<Observeur>> methodMap = this.evenementMap.get(evtClass);
+
+        if (methodMap == null) {
+            methodMap = new HashMap<>();
+
+            List<Observeur> observeurList = new ArrayList<>();
+            observeurList.add(o);
+
+            methodMap.put(method, observeurList);
+            return;
+        }
+
+        List<Observeur> observeurList = methodMap.get(method);
+        if (observeurList == null) {
+
+            observeurList = new ArrayList<>();
+            observeurList.add(o);
+            return;
+        }
+
+        observeurList.add(o);
+
+
+    }
+
+    /*
     public void record(Class<? extends Evenement> uneClass, Observeur o) {
         List<Observeur> observeursList = this.observeursMap.get(uneClass);
-
 
         if (observeursList == null) {
             observeursList = new ArrayList<>();
@@ -26,7 +56,26 @@ public class Observable {
 
         observeursList.add(o);
     }
+    */
 
+    public void signalPerform(Evenement evt, Method method, List<Object> objects) {
+        Map<Method, List<Observeur>> methodMap = this.evenementMap.get(evt.getClass());
+
+        if (methodMap != null) {
+
+            List<Observeur> observeurList = methodMap.get(method);
+
+            for (Observeur observeur : observeurList) {
+                try {
+                    method.invoke(objects);
+                } catch ( Exception e) {
+                    System.out.println("exeption invoke");
+                }
+            }
+        }
+    }
+
+    /*
     public void signal(Evenement evt) {
         List<Observeur> observeursList = this.observeursMap.get(evt.getClass());
 
@@ -37,4 +86,5 @@ public class Observable {
             }
         }
     }
+    */
 }
