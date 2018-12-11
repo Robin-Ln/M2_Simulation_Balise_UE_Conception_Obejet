23e39feb8d57a0d3bc0c783a3ccd915c926d88d9
Robin Louarn <robin.louarn@icloud.com>
2018-12-11 14:58:24 +0100

    test

---
+++ src/test/AnnouncerTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import observeur.Observable;
+import observeur.Observeur;
+import observeur.evenement.Evenement;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class AnnouncerTest {
+
+
+    @Test
+    public void test() {
+        TestObserveur observeur = new TestObserveur();
+        TestObservable observable = new TestObservable();
+
+        // enregistrement
+        observable.record(TestEvent.class, observeur);
+
+        // signal
+        TestEvent testEvent = new TestEvent(observable);
+        observable.signal(testEvent);
+
+        // signale reçus ?
+        assertTrue(testEvent.log.size() == 1);
+        assertTrue(testEvent.log.get(0).equals("class test.TestObservable -> class test.TestObserveur"));
+
+    }
+}
+
+class TestObserveur implements Observeur {}
+
+class TestObservable extends Observable {}
+
+
+class TestEvent extends Evenement<TestObservable, TestObserveur> {
+
+    List<String> log;
+
+
+    public TestEvent(TestObservable testObservable) {
+        super(testObservable);
+        this.log = new ArrayList<>();
+    }
+
+    @Override
+    public void traitement(TestObserveur testObserveur) {
+        this.log.add(this.getSource().getClass() + " -> " + testObserveur.getClass());
+    }
+}
