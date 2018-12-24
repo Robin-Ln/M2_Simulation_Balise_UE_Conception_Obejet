ebffaa70f0a6f2802e9f602184db397f9a9e5e33
Robin Louarn <robin.louarn@icloud.com>
2018-12-24 17:43:51 +0100

    Creation dynamique des balise et des satelite

--- src/vue/BaliseWorld.java
+++ src/vue/BaliseWorld.java
@@ -2,12 +2,12 @@ package vue;
 
 import graphicLayer.World;
 import modele.Balise;
-import modele.Entite;
 import modele.Modele;
 import modele.Sattelite;
-import observeur.evenement.SatteliteChange;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class BaliseWorld extends World {
 
@@ -33,11 +33,7 @@ public class BaliseWorld extends World {
     public BaliseWorld() {
         super("Simulation");
 
-        this.ciel = new CielVue(new Point(0, 0), new Dimension(BaliseWorld.width, BaliseWorld.height));
-        this.contents().add(this.ciel);
-
-        this.merVue = new MerVue(new Point(0, BaliseWorld.height), new Dimension(BaliseWorld.width, BaliseWorld.height));
-        this.contents().add(this.merVue);
+        this.creerTerrain();
 
         this.setPreferredSize(new Dimension(BaliseWorld.width, BaliseWorld.height * 2));
 
@@ -46,28 +42,63 @@ public class BaliseWorld extends World {
 
         // --------------
 
-        // Les entité
-        Balise balise = new Balise(modele,1950, BaliseWorld.height);
-        Balise balise2 = new Balise(modele,1000, BaliseWorld.height);
-        Balise balise3 = new Balise(modele, 500, BaliseWorld.height);
-        Balise balise4 = new Balise(modele, 700, BaliseWorld.height);
+        // keylistener
+        this.setKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                switch (e.getKeyChar()){
+                    case 'b':
+                        ajouterBalise();
+                        break;
+                    case 's':
+                        ajouterSatelite();
+                        break;
+                    case 'c':
+                        clearSimulation();
+                    default:
+                        break;
+                }
+            }
 
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
+
+    private void creerTerrain(){
+        this.ciel = new CielVue(new Point(0, 0), new Dimension(BaliseWorld.width, BaliseWorld.height));
+        this.contents().add(this.ciel);
+
+        this.merVue = new MerVue(new Point(0, BaliseWorld.height), new Dimension(BaliseWorld.width, BaliseWorld.height));
+        this.contents().add(this.merVue);
+    }
+
+    private void ajouterSatelite(){
         Sattelite sattelite = new Sattelite(modele,500, BaliseWorld.height - 100);
-        Sattelite sattelite2 = new Sattelite(modele,0, BaliseWorld.height - 150);
-
-
-        // Ajout des entite dans le modele (ajouter les satelites avant les balises )
         this.modele.getSattelites().add(sattelite);
-        this.modele.getSattelites().add(sattelite2);
+    }
 
+    private void ajouterBalise(){
+        Balise balise = new Balise(modele,1950, BaliseWorld.height);
         this.modele.getBalisese().add(balise);
-        this.modele.getBalisese().add(balise2);
-        this.modele.getBalisese().add(balise3);
-        this.modele.getBalisese().add(balise4);
+    }
+
+    private void clearSimulation(){
+        clear();
+
+        this.creerTerrain();
     }
 
 
     public void seDeplacer() {
         this.modele.seDeplacer();
     }
+
 }
